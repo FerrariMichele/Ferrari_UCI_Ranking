be4c33abdbb3f3eef036ab4abf1209110fb82e03
Michele Ferrari <Michele@0m3x7.onmicrosoft.com>
2024-05-03 21:23:38 +0200

    Aggiunda di scorrimento delle pagine di tutta la classifica

--- Ferrari_UCI_Ranking/Program.cs
+++ Ferrari_UCI_Ranking/Program.cs
@@ -20,55 +20,118 @@ namespace Ferrari_Selenium
 
         static void Main(string[] args)
         {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Get Riders");
+                Console.WriteLine("2 - Show Riders");
+                Console.WriteLine("3 - Exit");
+                Console.Write("Choose an option: ");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        GetRiders();
+                        break;
+                    case "2":
+                        string json = System.IO.File.ReadAllText("riders.json");
+                        List<Rider> riders = JsonConvert.DeserializeObject<List<Rider>>(json);
+                        foreach (var rider in riders)
+                        {
+                            Console.WriteLine("Rider Name: " + rider.RiderName);
+                            Console.WriteLine("Nationality: " + rider.Nationality);
+                            Console.WriteLine("Team: " + rider.Team);
+                            Console.WriteLine("Age: " + rider.Age);
+                            Console.WriteLine("Points: " + rider.Points);
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        keepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
+            }
+            
+
+        }
+
+        public static List<Rider> GetRiders()
+        {
             IWebDriver driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://dataride.uci.ch/iframe/RankingDetails/1?disciplineId=10&groupId=1&momentId=178357&disciplineSeasonId=432&rankingTypeId=1&categoryId=22&raceTypeId=0");
 
             Thread.Sleep(2000);
 
-            IWebElement tbodyElement = driver.FindElement(By.CssSelector("tbody[role='rowgroup']"));
+            IWebElement lastPageLink = driver.FindElement(By.CssSelector("a.k-pager-last"));
 
-            IReadOnlyCollection<IWebElement> rows = tbodyElement.FindElements(By.CssSelector("tr.k-master-row"));
+            string totalPagesString = lastPageLink.GetAttribute("data-page");
+
+            int totalPages;
+
+            if (int.TryParse(totalPagesString, out totalPages))
+            {
+                Console.WriteLine("Total number of data pages: " + totalPages);
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse total number of data pages.");
+                Environment.Exit(1);
+            }
 
             List<Rider> riders = new List<Rider>();
 
-            foreach (var row in rows)
+            for (int i = 0; i < totalPages; i++)
             {
-                string riderName = row.FindElement(By.CssSelector("td:nth-child(4) a")).Text;
-                string nationality = row.FindElement(By.CssSelector("td:nth-child(5)")).Text;
-                string team = row.FindElement(By.CssSelector("td:nth-child(6)")).Text;
-                string age = row.FindElement(By.CssSelector("td:nth-child(7)")).Text;
-                string points = row.FindElement(By.CssSelector("td.points")).Text;
 
-                riders.Add(new Rider
+                IWebElement tbodyElement = driver.FindElement(By.CssSelector("tbody[role='rowgroup']"));
+
+                IReadOnlyCollection<IWebElement> rows = tbodyElement.FindElements(By.CssSelector("tr.k-master-row"));
+
+                foreach (var row in rows)
                 {
-                    RiderName = riderName,
-                    Nationality = nationality,
-                    Team = team,
-                    Age = age,
-                    Points = points
-                });
+                    string riderName = row.FindElement(By.CssSelector("td:nth-child(4) a")).Text;
+                    Console.WriteLine(riderName);
+                    string nationality = row.FindElement(By.CssSelector("td:nth-child(5)")).Text;
+                    string team = row.FindElement(By.CssSelector("td:nth-child(6)")).Text;
+                    string age = row.FindElement(By.CssSelector("td:nth-child(7)")).Text;
+                    string points = row.FindElement(By.CssSelector("td.points")).Text;
+
+                    riders.Add(new Rider
+                    {
+                        RiderName = riderName,
+                        Nationality = nationality,
+                        Team = team,
+                        Age = age,
+                        Points = points
+                    });
+
+                }
+
+                if (i < totalPages - 1)
+                {
+                    IWebElement nextPageButton = driver.FindElement(By.CssSelector("[aria-label='Go to the next page']"));
+                    nextPageButton.Click();
+                    Thread.Sleep(1000);
+                }
             }
-
-            foreach (var rider in riders)
-            {
-                Console.WriteLine("Rider Name: " + rider.RiderName);
-                Console.WriteLine("Nationality: " + rider.Nationality);
-                Console.WriteLine("Team: " + rider.Team);
-                Console.WriteLine("Age: " + rider.Age);
-                Console.WriteLine("Points: " + rider.Points);
-                Console.WriteLine();
-            }
-
-            // Serialize the data to JSON
-            string json = JsonConvert.SerializeObject(riders, Formatting.Indented);
-
-            // Write JSON to file
-            string filePath = "riders.json";
-            System.IO.File.WriteAllText(filePath, json);
 
             Thread.Sleep(10000);
             driver.Quit();
+
+            string json = JsonConvert.SerializeObject(riders, Formatting.Indented);
+
+            string filePath = @"riders.json";
+            System.IO.File.WriteAllText(filePath, json);
+
+            return riders;
         }
     }
 }
